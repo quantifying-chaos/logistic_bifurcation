4e33e37eb229b340f1d168a233fd7e629b06507b
Harry Han <s2162783@ed.ac.uk>
2024-10-09 16:42:49 +0100

    bifurcation graph add line 1-1/r and improve resolution

--- src/logistic_bifurcation.py
+++ src/logistic_bifurcation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 
 #################
@@ -7,16 +8,16 @@ import matplotlib.pyplot as plt
 #################
 
 # Lower bound of r
-x_low = 2.5
+r_low = 2.5
 # Upper bound of r
-x_high = 4.0
+r_high = 4.0
+n_of_r = 3500  # Number of r values to try
 
 # init x
 x_0 = 0.5
 
 prep_times = 500  # Number of iterations to ignore
-plot_times = 1500  # Number of iterations to plot
-n_of_r = 2000  # Number of r values to try
+plot_times = 3000  # Number of iterations to plot
 
 
 def logistic(x, r):
@@ -44,24 +45,44 @@ marker_style = dict(linestyle=':', color='b',
 
 fig, ax = plt.subplots()
 plt.grid(color='gray', linestyle=':', linewidth=0.5)
-for r in np.linspace(x_low, x_high, n_of_r):
+
+r_vals = np.linspace(r_low, r_high, n_of_r)
+
+for r in r_vals:
+    # variable line size as for r <3.5 there are very few paths
+    l_size = 0.05
     if r < 3.5:  # before 3.5 the system is rather stable
         x = iterate_r(x_0, r, prep_times, 100)
+        l_size = 0.1
     else:
         x = iterate_r(x_0, r, prep_times, plot_times)
+        l_size = 0.05
 
     # plt.plot([r]*len(x), x, ',b')
     r_dummy = np.linspace(r, r, len(x))
-    ax.scatter(r_dummy, x, c='tab:blue', s=0.1,
-               alpha=0.90, edgecolors='none', facecolors='tab:blue',
+    ax.scatter(r_dummy, x, c='tab:blue', s=l_size,
+               alpha=0.1, edgecolors='none', facecolors='tab:blue',
                marker='o')
+
+ax.plot(r_vals, 1 - 1/r_vals, color='r',
+        linestyle='--', linewidth=0.8, alpha=0.5)
+
+# Custom legend
+legend_elements = [
+    Line2D([0], [0], color='r', linestyle='--', linewidth=0.8, label='1-1/r'),
+    Line2D([0], [0], marker='o', color='w', label='x values',
+           markerfacecolor='b', markersize=3),
+]
+
+
+ax.legend(handles=legend_elements, loc='upper left', fontsize=10)
 
 
 plt.xlabel('r', fontsize=15)
 plt.ylabel('x', fontsize=15)
 
-plt.xlim(x_low, x_high)
+plt.xlim(r_low, r_high)
 # plt.show()
 # plt.axis('off')
 # plt.gca().set_position([0, 0, 1, 1])
-plt.savefig("small_logistic_map.png", dpi=200)
+plt.savefig("bifurcation.png", dpi=2000)
